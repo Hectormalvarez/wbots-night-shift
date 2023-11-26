2faa6e80e5af1e497502d1afebdecbaeeb704751
Hector Alvarez <alvarez.hectorm@outlook.com>
2023-11-26 09:48:29 -0600

    commented out boilerplate

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import Header from "./Header";
-import HeroList, { HeroListItem } from "./HeroList";
-import TextInsertion from "./TextInsertion";
+// import Header from "./Header";
+// import HeroList, { HeroListItem } from "./HeroList";
+// import TextInsertion from "./TextInsertion";
 import { makeStyles } from "@fluentui/react-components";
-import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
+// import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
 
 interface AppProps {
   title: string;
@@ -15,30 +15,34 @@ const useStyles = makeStyles({
   },
 });
 
-const App = (props: AppProps) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const App = (_props: AppProps) => {
   const styles = useStyles();
   // The list items are static and won't change at runtime,
   // so this should be an ordinary const, not a part of state.
-  const listItems: HeroListItem[] = [
-    {
-      icon: <Ribbon24Regular />,
-      primaryText: "Achieve more with Office integration",
-    },
-    {
-      icon: <LockOpen24Regular />,
-      primaryText: "Unlock features and functionality",
-    },
-    {
-      icon: <DesignIdeas24Regular />,
-      primaryText: "Create and visualize like a pro",
-    },
-  ];
+  // const listItems: HeroListItem[] = [
+  //   {
+  //     icon: <Ribbon24Regular />,
+  //     primaryText: "Achieve more with Office integration",
+  //   },
+  //   {
+  //     icon: <LockOpen24Regular />,
+  //     primaryText: "Unlock features and functionality",
+  //   },
+  //   {
+  //     icon: <DesignIdeas24Regular />,
+  //     primaryText: "Create and visualize like a pro",
+  //   },
+  // ];
 
   return (
     <div className={styles.root}>
+
+      {/* 
       <Header logo="assets/logo-filled.png" title={props.title} message="Welcome" />
       <HeroList message="Discover what this add-in can do for you today!" items={listItems} />
       <TextInsertion />
+      */}
     </div>
   );
 };
